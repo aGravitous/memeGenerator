ba4202c644b1502cef6f6f01c0a1431f0f791ec1
Stéphane Dryefus <floatingrock@gmail.com>
2019-01-21 16:31:31 -0800

    Much style and design. No javascript yet.

---
+++ meme.js
@@ -0,0 +1,10 @@
+// Three columns? Enter image in central, see collection on right, text entry on left.
+// List accepted image types or convert?
+
+// Accept image.
+// Allow top and bottom text to be edited, display on central image.
+// Click done moves to collection. Export sends new image to desktop.
+// Thumbnail collection: clicking on one sends it back to center for more editing or export.
+// Reset for edit window simply clears text.
+// Reset for collection clears all.
+// Importing a new image before done clears and replaces.
