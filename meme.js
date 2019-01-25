c73a532cc2264eb66c77bddd6dd1df16ab49a47b
Stéphane Dryefus <floatingrock@gmail.com>
2019-01-24 20:32:38 -0800

    Now basically done. Images apeear, can be deleted.

--- meme.js
+++ meme.js
@@ -1,6 +1,6 @@
 // Styling notes: add scrollbar to results
-// Add delete to images
 // Get words in the right place.
+// Fix background of personal page.
 
 
 window.onload = function(){
@@ -14,20 +14,23 @@ var form = document.querySelector("form");
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
+    // Create html to hold image and words.
     var HTMLtoAppend = `
     <div class="meme">
-        <span>${topText.value}</span>
+        <span id="top-words">${topText.value}</span>
         <img src="${imgUrl.value}" alt="Your Image"></img>
-        <span>${bottomText.value}</span>
+        <span id="bottom-words">${bottomText.value}</span>
     </div>
     `;
+    // Create button for self delete.
     var button = document.createElement('button');
-    button.innerText = "Remove me!";
+    button.innerText = "Remove the above!";
+    button.classList.add('button');
     button.addEventListener('click', function(event){
         event.target.parentElement.remove()
     })
+    // Append button and add html and button back to page.
     var newDiv = document.createElement('div');
-
     newDiv.innerHTML = HTMLtoAppend;
     newDiv.append(button);
     document.getElementById('item3').append(newDiv);
